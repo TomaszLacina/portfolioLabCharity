6643df20441adb7f3a7fe553efd9943c2b07a557
Liwia Suchorab <82950405+liw94@users.noreply.github.com>
2021-12-21 13:59:43 +0100

    Create CategoryRepository.java

---
+++ src/main/java/pl/coderslab/charity/CategoryRepository.java
@@ -0,0 +1,9 @@
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.coderslab.FiszkoTeka.entity.Category;
+
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
